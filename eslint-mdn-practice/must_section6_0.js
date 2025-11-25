711b9e5713bf4e12af8b84a4c9d0460fa6de4f11
ninasings4women <nina.sings4women@gmail.com>
2025-11-25 13:04:19 +0900

    add work of section6-0

---
+++ eslint-mdn-practice/must_section6_0.js
@@ -0,0 +1,23 @@
+function plus(n1,n2){
+    return n1+n2;
+}
+
+const result = plus(3, 4);
+console.log(result);
+
+
+function f3(a1, b2){
+    if(a1 === "test" && b2 !== "temp") {
+        return b2;
+    } else {
+        return " " + b2 + "random text";
+    }
+}
+
+const resultString = f3("test", "sample");
+console.log(resultString)
+
+
+
+const catCount = 0;
+console.log(catCount);
